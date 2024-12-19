cc0a0fa6f0dd3bfd97f9c8471a703126517f6f42
Ashmit Nigam <ashmit.nigam1294@gmail.com>
2024-12-20 01:00:18 +0530

    path to all routes added

---
+++ FLEX_Backend/src/constants/path.ts
@@ -0,0 +1,12 @@
+// src/constants/path.ts
+const API_PATHS = {
+    BASE: '/api',
+    AUTH: {
+      BASE: '/auth',
+      REGISTER: '/register',
+      LOGIN: '/login',
+      PROFILE: '/profile'
+    }
+  };
+  
+export default API_PATHS;
