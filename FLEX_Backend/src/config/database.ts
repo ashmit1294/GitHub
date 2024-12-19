d09fcc9c336180999c70e3a19f9a89814a0792b1
Ashmit Nigam <ashmit.nigam1294@gmail.com>
2024-12-20 00:48:15 +0530

    Adding local db

--- FLEX_Backend/src/config/database.ts
+++ FLEX_Backend/src/config/database.ts
@@ -1,8 +1,12 @@
+import { error } from 'console';
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI || '');
+try {
+    if(process.env.MONGODB_URI){await mongoose.connect(process.env.MONGODB_URI);}
+    else{
+      throw error();
+    }
     console.log('MongoDB Connected Successfully');
   } catch (error) {
     console.error('MongoDB Connection Error:', error);
--- FLEX_Backend/src/server.ts
+++ FLEX_Backend/src/server.ts
@@ -13,7 +13,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
